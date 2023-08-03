62a8409659898b1cc3248d8dd9612db81287078f
Bladimir Minga <bminga@pegasus.cl>
2023-08-03 09:59:44 -0500

    Refactor gateway

--- microservice-gateway/src/main/java/com/bsmm/gateway/GatewayApplication.java
+++ microservice-gateway/src/main/java/com/bsmm/gateway/GatewayApplication.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -23,19 +24,26 @@ public class GatewayApplication {
     public RouteLocator routerBuilder(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 .route("user", r -> r.path("/users/**")
-                        .filters(f -> f.modifyRequestBody(EncryptedMessage.class, String.class, (exchange, originalRequest) -> {
-                                    log.info("originalRequest {}", originalRequest);
-                                    return originalRequest != null ?
-                                            Mono.just(Convert.decode(originalRequest.getPayload())) : Mono.empty();
-                                })
-                                .modifyResponseBody(String.class, EncryptedMessage.class, (exchange, originalResponse) -> {
-                                    log.info("originalResponse {}", originalResponse);
-                                    return originalResponse != null ?
-                                            Mono.just(new EncryptedMessage(Convert.encode(originalResponse))) : Mono.empty();
-                                }))
+                        .filters(this::setGatewayFilterSpec)
                         .uri("http://localhost:8081"))
                 .route("account", r -> r.path("/accounts/**")
+                        .filters(this::setGatewayFilterSpec)
                         .uri("http://localhost:8082")).build();
+    }
+
+    public GatewayFilterSpec setGatewayFilterSpec(GatewayFilterSpec f) {
+        return f.modifyRequestBody(EncryptedMessage.class, String.class, (exchange, originalRequest) -> {
+                    log.info("originalRequest {}", originalRequest);
+                    return originalRequest != null ?
+                            Mono.just(Convert.decode(originalRequest.getPayload())) : Mono.empty();
+                })
+                .modifyResponseBody(String.class, EncryptedMessage.class, (exchange, originalResponse) -> {
+                    log.info("originalResponse {}", originalResponse);
+                    return originalResponse != null ?
+                            Mono.just(new EncryptedMessage(Convert.encode(originalResponse))) : Mono.empty();
+                })
+                .addRequestHeader("test", "test")
+                .addResponseHeader("key", "encrypted-key");
     }
 
     @Bean
